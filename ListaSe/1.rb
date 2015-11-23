b67b6fa9c3d713ebde54888f9e4819c27a394000
Evandson Maranhão <evandson.cm@gmail.com>
2015-11-23 19:47:58 -0200

    Commit Lista I

---
+++ ListaSe/1.rb
@@ -0,0 +1,8 @@
+puts "Informe um numero: "
+n = gets.to_i
+
+if(n == 10) then
+	puts "O numero é igual a 10"
+else
+	puts "NÃO é igual a 10"
+end
---
+++ ListaSe/2.rb
@@ -0,0 +1,8 @@
+puts "Informe um numero: "
+n = gets.to_i
+
+if (n % 7 == 0) then
+	puts "É múltiplo de 7"
+else
+	puts "NÃO é múltiplo de 7"
+end
